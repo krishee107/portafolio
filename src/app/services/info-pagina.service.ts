36aa9127e43f4b0451f0c67b20f1a1759ca0adc4
Cristina <cristinaferrerartigas@gmail.com>
2022-07-01 20:19:16 +0200

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,10 +9,16 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any = {};
 
   constructor( private http: HttpClient ) {
-    console.log('Servicio de infoPagina listo')
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+   }
 
+   private cargarInfo(){
+    console.log('Servicio de infoPagina listo')
 
     //Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
@@ -20,5 +26,17 @@ export class InfoPaginaService {
         this.cargada = true;
         this.info = resp;
       })
+   }
+
+   private cargarEquipo(){
+    console.log('Servicio de firebase listo')
+
+    //Leer archivo JSON Firebase
+    this.http.get('https://angular-html-57ecc-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any) =>{
+        this.equipo = resp;
+        console.log(resp)
+      })
+
    }
 }
